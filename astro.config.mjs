4277857d3073e1f61a1a78ee29ad14666e3177f7
Sergio Majé <smajefranco@gmail.com>
2024-10-31 12:00:03 -0500

    🔧 Set MDX add-ons

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,31 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), mdx(), react()]
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [
+        // Add IDs to headings
+        rehypeSlug,
+        [
+          /**
+           * Enhances code blocks with syntax highlighting, line numbers,
+           * titles, and allows highlighting specific lines and words
+           */
+          rehypePrettyCode,
+          {
+            theme: 'github-dark',
+          }
+        ]
+      ]
+    }),
+    react()
+  ]
 });
